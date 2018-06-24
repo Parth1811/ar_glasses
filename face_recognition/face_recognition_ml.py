9fe90812baf93b94bb9fcd4b985a55b56cddd432
Parth <parthvin@gmail.com>
2018-06-24 17:59:49 +0530

    working face_recognition using dlib ml

--- face_recognition/face_recognition_ml.py
+++ face_recognition/face_recognition_ml.py
@@ -12,45 +12,37 @@ import face_ml_api as api
 face_encodings_array = []
 
 def run(data):
-    start_time = datetime.now()
-    print start_time
     if data["first_run"]:
         #frames, labels = database.read_all_images(data,"char")
-        frames = [
-        api.load_image_file('/home/parth/ar_glasses/face_recognition/database/dhananjay/images/16.jpg'),
-        api.load_image_file('/home/parth/ar_glasses/face_recognition/database/parth/images/16.jpg'),
-        api.load_image_file('/home/parth/ar_glasses/face_recognition/database/saavi/images/0.jpg'),
-        api.load_image_file('/home/parth/ar_glasses/face_recognition/database/rutvi/images/1.jpg')
-        ]
+        paths, labels = database.read_single_images(data, path_instead_of_frame = True)
 
-        labels = [
-        "dhananjay",
-        "parth",
-        "saavi",
-        "rutvi"
-        ]
-
-
-        for frame in frames:
+        for path in paths:
+            frame = api.load_image_file(path)
             face_encodings_array.append(api.face_encodings(frame)[0])
         data["first_run"] = False
 
     face_encoding_current, matches = [], []
-    print (datetime.now()-start_time).total_seconds()
     video_feed = camera_driver.cam_read(data["camera"])
     small_frame = cv2.resize(video_feed["frame"], (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
     face_location = api.face_locations(rgb_small_frame)
-    face_encoding_current = api.face_encodings(rgb_small_frame, face_location)
-    face_names = []
-    for face_encoding in face_encoding_current:
-        matches = api.compare_faces(face_encodings_array, face_encoding)
-        name = "Unknown"
+    if face_location != []:
+        data["is_face_present"] = True
+        data["location"] = face_location
+        face_encoding_current = api.face_encodings(rgb_small_frame, face_location)
+        for face_encoding in face_encoding_current:
+            matches = api.compare_faces(face_encodings_array, face_encoding)
 
-        if True in matches:
-            first_match_index = matches.index(True)
-            print first_match_index
+            if True in matches:
+                first_match_index = matches.index(True)
+                data["face_info"] = database.read_info(index = first_match_index)
+            else:
+                data["face_info"] = {"full name" : "No match"}
+    else:
+        data["is_face_present"] = False
+        try:
+            del data["location"], data["face_info"]
+        except :
+            pass
 
-        face_names.append(name)
-    print face_names
     return data
